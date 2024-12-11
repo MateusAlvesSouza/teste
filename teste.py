3b5ccdd51973fe07d1c52e26235354f74358ffa3
MateusAlvesSouza <mateus.e.souza@escola.pr.gov.br>
2024-12-11 19:20:10 -0300

    adicionar os outros arquivos de teste

---
+++ teste.py
@@ -0,0 +1,5 @@
+def converca(nome, idade, formacao):
+    return f'meu nome é {nome}, tenho {idade} anos, minha formação atual é {formacao}'
+
+c = converca('Mateus Souza', 16, 'curso o ensino médio' )
+print(c)
